a0f19431bc1a8685dab08de2c3783443260e2cd8
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-25 01:47:09 +0600

    Added contactsReducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit";
+import {contactsReducer} from '../containers/Contact/contactSlice';
 
 export const  store = configureStore({
     reducer: {
+      contacts: contactsReducer,
     },
 });
 export type RootState = ReturnType<typeof store.getState>;
