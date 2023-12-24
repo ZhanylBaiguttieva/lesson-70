1d988dae88b679850e77bd5b88e17e8e5335b090
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-25 01:47:14 +0600

    Added Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import './App.css';
+import NavBar from './NavBar/NavBar';
+import {Route, Routes} from 'react-router-dom';
+import Contacts from './containers/Contact/Contacts';
+import NewContact from './containers/NewContact/NewContact';
 
 function App() {
 
   return (
-    <div></div>
+    <div>
+      <header>
+        <NavBar/>
+      </header>
+      <main className="container-fluid">
+        <Routes>
+          <Route path="/" element={(<Contacts/>)} />
+          <Route path="/new-contact" element={(<NewContact/>)} />
+          <Route path="*" element={(<h1>Not Found!</h1>)}/>
+        </Routes>
+
+      </main>
+    </div>
   );
 }
 
