c4dba90c92af3d1f0cd8b070c06b93323b6a9dd5
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-25 03:19:40 +0600

    Added Route to edit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import NavBar from './NavBar/NavBar';
 import {Route, Routes} from 'react-router-dom';
 import Contacts from './containers/Contact/Contacts';
 import NewContact from './containers/NewContact/NewContact';
+import EditContact from './containers/EditContact/EditContact';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={(<Contacts/>)} />
           <Route path="/new-contact" element={(<NewContact/>)} />
+          <Route path="/edit-contact/:id" element={(<EditContact/>)} />
           <Route path="*" element={(<h1>Not Found!</h1>)}/>
         </Routes>
 
