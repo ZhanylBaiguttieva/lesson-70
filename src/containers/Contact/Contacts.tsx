5712786ddb4eb28760f7889fc81c6a57048ea080
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-25 01:08:36 +0600

    Added component Contacts.tsx

---
+++ src/containers/Contact/Contacts.tsx
@@ -0,0 +1,31 @@
+import {useAppDispatch, useAppSelector} from '../../app/hooks';
+import {selectContacts, selectFetchContactLoading} from './contactSlice';
+import React, {useEffect} from 'react';
+import {fetchContacts} from './contactThunks';
+import Spinner from '../../Spinners/Spinner';
+import ContactItem from './ContactItem';
+import {Contact} from '../../types';
+
+const Contacts: React.FC= () => {
+  const dispatch = useAppDispatch();
+  const contacts = useAppSelector(selectContacts);
+  const contactsLoading = useAppSelector(selectFetchContactLoading);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <h4>Dishes</h4>
+      {contactsLoading ? <Spinner/> : contacts.map((contact: Contact) => (
+        <ContactItem
+          key={contact.id}
+          contact={contact}
+        />
+      ))}
+    </>
+  );
+};
+
+export default Contacts;
