363921e0292d9ac8f068f58113b6439f207d0a02
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-25 15:40:36 +0600

    Added Navlink Contacts

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -4,8 +4,7 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container-fluid">
-        <span className="navbar-brand">Contacts</span>
-
+        <NavLink to="/" className="nav-link fs-5 text-white">Contacts</NavLink>
         <ul className="navbar-nav mr-auto flex-row gap-2 flex-nowrap">
           <li className="nav-item">
             <NavLink to="/new-contact" className="nav-link">New Contact</NavLink>
